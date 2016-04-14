6a28588dbc713d6c19e76fa47620148b491206d0
liapedro <pedro_1288@hotmail.com>
2016-04-14 12:50:30 -0600

    Uso de variables en formularios
    Permitimos la edición de un formulario utilizando $scope y funciones
    dentro del controlador, (funciones EditarProfesor, GuardarCambios,
    CancelarCambios).

---
+++ practica02/js/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('universidadApp', [ ]);
+
+//Controlador Profesor
+app.controller('profesorCtrl', function ($scope) {
+	
+	$scope.profesor = profesorData;
+	$scope.editando = { };
+
+	$scope.EditarProfesor = function(){
+		angular.copy( $scope.profesor, $scope.editando );
+	}
+
+	$scope.GuardarCambios = function(){
+		angular.copy( $scope.editando, $scope.profesor );
+	}
+
+	$scope.CancelarCambios = function(){
+		$scope.editando = {};
+	}
+
+});
+
+var profesorData = {
+	nombre: "Pedro Daniel Rojas Corona",
+	bio: "Saludos estudiante, mi nombre es Pedro Daniel, encantado de conocerte, soy una apasionado instructor de matemáticas aplicadas cuánticas, más orientado a la física termonuclear. Mi vocación es ser maestro y lograr transmitir mis conocimientos a todos mis estudiantes!.",
+	edad: 27,
+	foto: "img/juancarlos.jpg"
+}
