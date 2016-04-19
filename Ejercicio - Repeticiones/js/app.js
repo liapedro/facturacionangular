557c1cddd776daeaab0f66401f9b0bdb3439bf35
liapedro <pedro_1288@hotmail.com>
2016-04-19 12:03:47 -0600

    Segundo Ejercicio con repeticiones
    Creamos una tabla donde la llenamos con ng-repeat tomando los datos a
    través de un arreglo dentro del app.js

--- Ejercicio - Repeticiones/js/app.js
+++ Ejercicio - Repeticiones/js/app.js
@@ -6,6 +6,24 @@ app.controller('listadoCtrl', ['$scope', function($scope){
 	
 		$scope.listado = ["Fernando Herrera", "Melisa Flores", "Pedro Daniel"];
 
+		$scope.listadoProfesores = {
+			profesores: [{
+				nombre: "Pedro Daniel",
+				edad: 27,
+				clase: "Física cuántica avanzada"
+			},
+			{
+				nombre: "Miguel Ulises",
+				edad: 24,
+				clase: "Filosofía"
+			},
+			{
+				nombre: "Ivan Alberto",
+				edad: 21,
+				clase: "Matemáticas"
+			}]
+		}
+
 
 
 }]);
