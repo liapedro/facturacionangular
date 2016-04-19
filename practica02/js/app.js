5a53737352940d1887656783f2b6f875290d0f97
liapedro <pedro_1288@hotmail.com>
2016-04-18 19:39:00 -0700

    Ng-hide / Ng-show
    Mostramos y ocultamos un formulario con la opcion de ng-hide y ng-show

--- practica02/js/app.js
+++ practica02/js/app.js
@@ -5,17 +5,22 @@ app.controller('profesorCtrl', function ($scope) {
 	
 	$scope.profesor = profesorData;
 	$scope.editando = { };
+	$scope.mostrarCaja = false;
+
 
 	$scope.EditarProfesor = function(){
 		angular.copy( $scope.profesor, $scope.editando );
+		$scope.mostrarCaja = true;
 	}
 
 	$scope.GuardarCambios = function(){
 		angular.copy( $scope.editando, $scope.profesor );
+		$scope.mostrarCaja = false;
 	}
 
 	$scope.CancelarCambios = function(){
 		$scope.editando = {};
+		$scope.mostrarCaja = false;
 	}
 
 });
