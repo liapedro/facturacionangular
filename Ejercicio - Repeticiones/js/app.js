25eee19d05efabdb40053ccaeaf15aa1e1afae7e
liapedro <pedro_1288@hotmail.com>
2016-04-19 09:03:05 -0600

    Ejercicios con NG-repeat
    Ejercicio utilizando ng-repeat, donde mostramos en forma de lista el
    contenido de un arreglo a través de json.

---
+++ Ejercicio - Repeticiones/js/app.js
@@ -0,0 +1,17 @@
+(function(){
+
+var app = angular.module('universidadApp',[ ]);
+
+app.controller('listadoCtrl', ['$scope', function($scope){
+	
+		$scope.listado = ["Fernando Herrera", "Melisa Flores", "Pedro Daniel"];
+
+
+
+}]);
+
+
+
+
+
+})();
